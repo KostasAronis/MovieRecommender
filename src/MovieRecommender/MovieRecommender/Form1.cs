299f8dd49a82e8ccce9551c6ee946758a4d29bbc
Alexandros <azaridak@gmail.com>
2019-11-09 20:58:15 +0200

    test com

--- src/MovieRecommender/MovieRecommender/Form1.cs
+++ src/MovieRecommender/MovieRecommender/Form1.cs
@@ -16,5 +16,10 @@ namespace MovieRecommender
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("test push");
+        }
     }
 }
